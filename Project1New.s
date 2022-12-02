581c845f5128f137ca14e23c6d7893eff336ecc0
Kenneth Gollaher <86015400+Kenneth-Gollaher@users.noreply.github.com>
2022-12-02 01:17:59 -0500

    Project One Submission
    For this project, you will take the existing binary file (O file) for the client management application, convert it to assembly code, convert the assembly code to C++ (CPP file) with an added output statement, and create a new binary file.
    
    1. Convert the binary file (O file) into assembly code.
    - Decompile the binary file found in the Software Reverse Engineering Playground in the Project One Files folder in Codio.
    2. Explain the functionality of the blocks of assembly code.
    - Use the Project One Proficiency Test Template, located in the Supporting Materials section, to complete this step.
    3. Convert the assembly code into C++ code (CPP file).
    - Compile the C++ code in the Eclipse IDE.
    4. Add an output statement to the beginning of the C++ code (CPP file).
    - For example, you could add a “Created by (name)” statement for when the program opens, or a statement of your choice.
    5. Convert the CPP file to a new binary file (O file).

---
+++ Project1New.s
@@ -0,0 +1,638 @@
+	.file	"Project1New.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+.LC0:
+	.string	""
+.LC1:
+	.string	"Enter your username: \n"
+.LC2:
+	.string	"Enter your password: \n"
+.LC3:
+	.string	"123"
+	.text
+	.globl	_Z25CheckUserPermissionAccessv
+	.type	_Z25CheckUserPermissionAccessv, @function
+_Z25CheckUserPermissionAccessv:
+.LFB1493:
+	.cfi_startproc
+	.cfi_personality 0x9b,DW.ref.__gxx_personality_v0
+	.cfi_lsda 0x1b,.LLSDA1493
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$104, %rsp
+	.cfi_offset 3, -24
+	movq	%fs:40, %rax
+	movq	%rax, -24(%rbp)
+	xorl	%eax, %eax
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcEC1Ev@PLT
+	leaq	-97(%rbp), %rdx
+	leaq	-96(%rbp), %rax
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+.LEHB0:
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1EPKcRKS3_@PLT
+.LEHE0:
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcED1Ev@PLT
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcEC1Ev@PLT
+	leaq	-97(%rbp), %rdx
+	leaq	-64(%rbp), %rax
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+.LEHB1:
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1EPKcRKS3_@PLT
+.LEHE1:
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcED1Ev@PLT
+	leaq	.LC1(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+.LEHB2:
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-96(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZStrsIcSt11char_traitsIcESaIcEERSt13basic_istreamIT_T0_ES7_RNSt7__cxx1112basic_stringIS4_S5_T1_EE@PLT
+	leaq	.LC2(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZStrsIcSt11char_traitsIcESaIcEERSt13basic_istreamIT_T0_ES7_RNSt7__cxx1112basic_stringIS4_S5_T1_EE@PLT
+.LEHE2:
+	leaq	-64(%rbp), %rax
+	leaq	.LC3(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_
+	testb	%al, %al
+	je	.L2
+	movl	$1, %ebx
+	jmp	.L3
+.L2:
+	movl	$2, %ebx
+.L3:
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+	leaq	-96(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+	movl	%ebx, %eax
+	movq	-24(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L9
+	jmp	.L13
+.L10:
+	movq	%rax, %rbx
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcED1Ev@PLT
+	movq	%rbx, %rax
+	movq	%rax, %rdi
+.LEHB3:
+	call	_Unwind_Resume@PLT
+.L11:
+	movq	%rax, %rbx
+	leaq	-97(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSaIcED1Ev@PLT
+	jmp	.L7
+.L12:
+	movq	%rax, %rbx
+	leaq	-64(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+.L7:
+	leaq	-96(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEED1Ev@PLT
+	movq	%rbx, %rax
+	movq	%rax, %rdi
+	call	_Unwind_Resume@PLT
+.LEHE3:
+.L13:
+	call	__stack_chk_fail@PLT
+.L9:
+	addq	$104, %rsp
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1493:
+	.globl	__gxx_personality_v0
+	.section	.gcc_except_table,"a",@progbits
+.LLSDA1493:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 .LLSDACSE1493-.LLSDACSB1493
+.LLSDACSB1493:
+	.uleb128 .LEHB0-.LFB1493
+	.uleb128 .LEHE0-.LEHB0
+	.uleb128 .L10-.LFB1493
+	.uleb128 0
+	.uleb128 .LEHB1-.LFB1493
+	.uleb128 .LEHE1-.LEHB1
+	.uleb128 .L11-.LFB1493
+	.uleb128 0
+	.uleb128 .LEHB2-.LFB1493
+	.uleb128 .LEHE2-.LEHB2
+	.uleb128 .L12-.LFB1493
+	.uleb128 0
+	.uleb128 .LEHB3-.LFB1493
+	.uleb128 .LEHE3-.LEHB3
+	.uleb128 0
+	.uleb128 0
+.LLSDACSE1493:
+	.text
+	.size	_Z25CheckUserPermissionAccessv, .-_Z25CheckUserPermissionAccessv
+	.section	.rodata
+	.align 8
+.LC4:
+	.string	"Client's Name    Service Selected (1 = Brokerage, 2 = Retirement)"
+.LC5:
+	.string	"\n"
+.LC6:
+	.string	"1. "
+.LC7:
+	.string	"Bob Jones"
+.LC8:
+	.string	" selected option "
+.LC9:
+	.string	"2. "
+.LC10:
+	.string	"Sarah Davis"
+.LC11:
+	.string	"3. "
+.LC12:
+	.string	"Amy Friendly"
+.LC13:
+	.string	"4. "
+.LC14:
+	.string	"Johnny Smith"
+.LC15:
+	.string	"5. "
+.LC16:
+	.string	"Carol Spears"
+	.text
+	.globl	_Z11DisplayInfoRiS_S_S_S_
+	.type	_Z11DisplayInfoRiS_S_S_S_, @function
+_Z11DisplayInfoRiS_S_S_S_:
+.LFB1494:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	%rdx, -24(%rbp)
+	movq	%rcx, -32(%rbp)
+	movq	%r8, -40(%rbp)
+	leaq	.LC4(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC6(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC7(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC8(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC9(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC10(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC8(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC11(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC12(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC8(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC13(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC14(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC8(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-32(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC15(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC16(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC8(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-40(%rbp), %rax
+	movl	(%rax), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1494:
+	.size	_Z11DisplayInfoRiS_S_S_S_, .-_Z11DisplayInfoRiS_S_S_S_
+	.section	.rodata
+	.align 8
+.LC17:
+	.string	"Enter the number of the client that you wish to change\n"
+	.align 8
+.LC18:
+	.string	"Please enter the client's new service choice (1 = Brokerage, 2 = Retirement)\n"
+	.text
+	.globl	_Z20ChangeCustomerChoiceRiS_S_S_S_
+	.type	_Z20ChangeCustomerChoiceRiS_S_S_S_, @function
+_Z20ChangeCustomerChoiceRiS_S_S_S_:
+.LFB1495:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$64, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	%rcx, -48(%rbp)
+	movq	%r8, -56(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -16(%rbp)
+	movl	$0, -12(%rbp)
+	leaq	.LC17(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC18(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	movl	-16(%rbp), %eax
+	cmpl	$1, %eax
+	jne	.L17
+	movl	-12(%rbp), %edx
+	movq	-24(%rbp), %rax
+	movl	%edx, (%rax)
+	jmp	.L24
+.L17:
+	movl	-16(%rbp), %eax
+	cmpl	$2, %eax
+	jne	.L19
+	movl	-12(%rbp), %edx
+	movq	-32(%rbp), %rax
+	movl	%edx, (%rax)
+	jmp	.L24
+.L19:
+	movl	-16(%rbp), %eax
+	cmpl	$3, %eax
+	jne	.L20
+	movl	-12(%rbp), %edx
+	movq	-40(%rbp), %rax
+	movl	%edx, (%rax)
+	jmp	.L24
+.L20:
+	movl	-16(%rbp), %eax
+	cmpl	$4, %eax
+	jne	.L21
+	movl	-12(%rbp), %edx
+	movq	-48(%rbp), %rax
+	movl	%edx, (%rax)
+	jmp	.L24
+.L21:
+	movl	-16(%rbp), %eax
+	cmpl	$5, %eax
+	jne	.L24
+	movl	-12(%rbp), %edx
+	movq	-56(%rbp), %rax
+	movl	%edx, (%rax)
+	nop
+.L24:
+	nop
+	movq	-8(%rbp), %rax
+	xorq	%fs:40, %rax
+	je	.L23
+	call	__stack_chk_fail@PLT
+.L23:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1495:
+	.size	_Z20ChangeCustomerChoiceRiS_S_S_S_, .-_Z20ChangeCustomerChoiceRiS_S_S_S_
+	.section	.rodata
+	.align 8
+.LC19:
+	.string	"Created by: Kenneth J Gollaher\n"
+	.align 8
+.LC20:
+	.string	"Hello! Welcome to our Investment Company\n"
+	.align 8
+.LC21:
+	.string	"Invalid Password. Please try again\n"
+.LC22:
+	.string	"What would you like to do?\n"
+	.align 8
+.LC23:
+	.string	"DISPLAY the client list (enter 1)\n"
+	.align 8
+.LC24:
+	.string	"CHANGE a client's choice (enter 2)\n"
+.LC25:
+	.string	"Exit the program.. (enter 3)\n"
+.LC26:
+	.string	"You chose "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1496:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -12(%rbp)
+	movl	$0, -36(%rbp)
+	movl	$1, -32(%rbp)
+	movl	$2, -28(%rbp)
+	movl	$1, -24(%rbp)
+	movl	$1, -20(%rbp)
+	movl	$2, -16(%rbp)
+	leaq	.LC19(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC20(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+.L28:
+	cmpl	$1, -12(%rbp)
+	je	.L34
+	call	_Z25CheckUserPermissionAccessv
+	movl	%eax, -12(%rbp)
+	cmpl	$1, -12(%rbp)
+	je	.L36
+	leaq	.LC21(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	jmp	.L28
+.L36:
+	nop
+.L34:
+	movl	-36(%rbp), %eax
+	cmpl	$3, %eax
+	je	.L29
+	leaq	.LC22(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC23(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC24(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	.LC25(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-36(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC26(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movl	-36(%rbp), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC5(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movl	-36(%rbp), %eax
+	cmpl	$1, %eax
+	jne	.L30
+	leaq	-16(%rbp), %rdi
+	leaq	-20(%rbp), %rcx
+	leaq	-24(%rbp), %rdx
+	leaq	-28(%rbp), %rsi
+	leaq	-32(%rbp), %rax
+	movq	%rdi, %r8
+	movq	%rax, %rdi
+	call	_Z11DisplayInfoRiS_S_S_S_
+	jmp	.L34
+.L30:
+	movl	-36(%rbp), %eax
+	cmpl	$2, %eax
+	jne	.L32
+	leaq	-16(%rbp), %rdi
+	leaq	-20(%rbp), %rcx
+	leaq	-24(%rbp), %rdx
+	leaq	-28(%rbp), %rsi
+	leaq	-32(%rbp), %rax
+	movq	%rdi, %r8
+	movq	%rax, %rdi
+	call	_Z20ChangeCustomerChoiceRiS_S_S_S_
+	jmp	.L34
+.L32:
+	movl	-36(%rbp), %eax
+	cmpl	$3, %eax
+	jne	.L34
+	movl	$1, %eax
+	jmp	.L33
+.L29:
+	movl	$0, %eax
+.L33:
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L35
+	call	__stack_chk_fail@PLT
+.L35:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1496:
+	.size	main, .-main
+	.section	.text._ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_,"axG",@progbits,_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_,comdat
+	.weak	_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_
+	.type	_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_, @function
+_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_:
+.LFB1737:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE7compareEPKc@PLT
+	testl	%eax, %eax
+	sete	%al
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1737:
+	.size	_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_, .-_ZSteqIcSt11char_traitsIcESaIcEEbRKNSt7__cxx1112basic_stringIT_T0_T1_EEPKS5_
+	.text
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1984:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L41
+	cmpl	$65535, -8(%rbp)
+	jne	.L41
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L41:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1984:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z25CheckUserPermissionAccessv, @function
+_GLOBAL__sub_I__Z25CheckUserPermissionAccessv:
+.LFB1985:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1985:
+	.size	_GLOBAL__sub_I__Z25CheckUserPermissionAccessv, .-_GLOBAL__sub_I__Z25CheckUserPermissionAccessv
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z25CheckUserPermissionAccessv
+	.hidden	DW.ref.__gxx_personality_v0
+	.weak	DW.ref.__gxx_personality_v0
+	.section	.data.rel.local.DW.ref.__gxx_personality_v0,"awG",@progbits,DW.ref.__gxx_personality_v0,comdat
+	.align 8
+	.type	DW.ref.__gxx_personality_v0, @object
+	.size	DW.ref.__gxx_personality_v0, 8
+DW.ref.__gxx_personality_v0:
+	.quad	__gxx_personality_v0
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
